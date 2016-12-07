84986518ed61d573f26345551129cb3ea2bdca31
Andrew McClure <andrew@southweb.co.nz>
2016-12-08 12:11:09 +1300

    Added write(buffer,len);

--- linux-serial.c
+++ linux-serial.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 LinuxSerial l;
+const char * message = "What the fuck";
 
 int main(int argc, char **argv){
 	if (!l.begin(9600))
@@ -13,6 +14,9 @@ int main(int argc, char **argv){
 			printf("%c",j);
 			fflush(stdout);
 			}
+		if (j == '?')
+			//l.write((uint8_t)'c');
+			l.write((uint8_t*)message,13);
 		}
 	}
 
--- linux-serial.h
+++ linux-serial.h
@@ -23,16 +23,15 @@ class LinuxSerial : public  Stream {
         int end() { return serialport_close(fd);};
 	int available() {return pread(fd,buf,1,0); } ;
         int read() {return serialport_read_until(fd, buf, NULL, 1,1) >=0 ? buf[0] :-1;}; 
-        size_t write(uint8_t b ){return  serialport_writebyte(fd, b) ==0;};
+        size_t write(uint8_t b ){return  (serialport_writebyte(fd, b) >=0);};
 	int peek() {int l = pread(fd,buf,1,0); return l>=0? buf[0]:-1;} ;
         size_t write(const uint8_t * data, int len) {
 		int l = 0;
 		uint8_t *p = (uint8_t*)data;
 		while(l < len){
-			if (!write(*(p++))) {
+			if (!this->write(*(p++))) 
 				break;
 			l++;
-			}
 		}
 		return (size_t)l;
 	 };
